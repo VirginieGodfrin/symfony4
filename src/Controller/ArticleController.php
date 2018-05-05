3720ddb090ee3dddc092efd111b89e996a416621
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-05-05 14:27:03 +0200

    Chapter 6: Twig ❤

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController { 
+class ArticleController extends AbstractController { 
 
 	/**
 	 * @Route("/")
@@ -21,9 +22,16 @@ class ArticleController {
 	* @Route("/news/{slug}")
 	*/
 	public function show($slug)
-	{
-		//sprintf — Retourne une chaîne formatée
-		// return new Response(sprintf('Future page to show the article: "%s"', $slug));
-		return new Response('Future page to show the article:'. $slug);
+	{	
+		$comments = [
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos tenetur dolorum corporis consectetur repudiandae quasi molestias rem iste ut ullam inventore deleniti quisquam, eum maxime quia reprehenderit quis accusamus eaque.',
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat eum accusantium eaque cum a ipsum accusamus ipsa natus molestiae quis nobis ipsam et obcaecati fugiat iste quod, magni ad non.',
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque voluptas labore ea unde incidunt. Facilis assumenda, error placeat commodi, aliquam praesentium quia labore voluptatem quisquam sunt non itaque quasi fuga!'
+		];
+
+		return $this->render('article/show.html.twig', [
+			'title' => $slug,
+			'comments' => $comments
+		]);
 	}
 }
